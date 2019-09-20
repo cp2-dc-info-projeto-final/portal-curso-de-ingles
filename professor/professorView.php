28c232506f14e45a2d36c0bca56019ea225e01e4
Elizabeth Gostosa <thesaekobr@gmail.com>
2019-09-20 11:51:54 -0300

    adicionando pagina do professor

---
+++ professor/professorView.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Página do aluno</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Página do professor</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <a href="visualizacao/verQuestoesView.php"><img src="aula.jpg"></a>
+      <p>visualizar questoes com gabarito</p>
+      <p>beterraba ipsum dolor.</p>
+    </div>
+    <div class="col-sm-4">
+    <a href="envio/enviarView.php"><img src="aula.jpg"></a>
+      <p>criar questoes</p>
+      <p>hipopotama quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+    <a href="visualizacao/verAlunosView.php"><img src="aula.jpg"></a>        
+      <p>visualizar alunos</p>
+      <p>FUS RO DAH</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
