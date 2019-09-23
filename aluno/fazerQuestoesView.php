8648ade836c5636a242f740b7fb71f77e9a72f87
taisinha <telma_evangelista@yahoo.com.br>
2019-09-23 14:38:19 -0300

    Alterações que a Elizabeth fez para melhorar o funcionamento

--- aluno/fazerQuestoesView.php
+++ aluno/fazerQuestoesView.php
@@ -5,6 +5,8 @@
     </head>
 <body>
 
+<form action="fazerQuestoesCtrl.php" class="needs-validation" novalidate method="post">
+
 <?php
    require_once('fazerQuestoesCtrl.php');
 
@@ -16,12 +18,17 @@
         
         echo "<li>" . $questao['opA'] . "</li>";
         echo "<input type='radio' name='q_" . $questao['idquestao'] . "' value='A' checked /> A";
+        echo "<li>" . $questao['opB'] . "</li>";
         echo "<input type='radio' name='q_" . $questao['idquestao'] . "' value='B'/> B";
+        echo "<li>" . $questao['opC'] . "</li>";
         echo "<input type='radio' name='q_" . $questao['idquestao'] . "' value='C'/> C";
+        echo "<li>" . $questao['opD'] . "</li>";
         echo "<input type='radio' name='q_" . $questao['idquestao'] . "' value='D'/> D";
         
         echo "</ol>";
     }
 ?>
+<button type="submit" class="btn btn-primary">Submit</button>
+</form>
 </body>
 </html>
