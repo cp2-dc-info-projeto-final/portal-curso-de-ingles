a3f30ded74aacbdd78fdb916985b5638a07ec2c1
Lab Caxias <cp2dcds@outlook.com>
2019-10-04 07:33:03 -0300

    banco de dados corrigido pela 20ª vez

--- portalcursodeingles.sql
+++ portalcursodeingles.sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 31-Ago-2019 às 00:54
+-- Generation Time: 04-Out-2019 às 12:30
 -- Versão do servidor: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -30,10 +30,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `aula` (
   `idaula` int(11) NOT NULL,
-  `data_aula` date,
-  `aluno` int(11),
-  `professor` varchar(50)
-);
+  `data_aula` date DEFAULT NULL,
+  `aluno` int(11) DEFAULT NULL,
+  `professor` varchar(50) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -43,43 +43,8 @@ CREATE TABLE `aula` (
 
 CREATE TABLE `exercicio` (
   `idexercicio` int(11) NOT NULL,
-  `nome` varchar(50) 
-  );
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `palavra`
---
-
-CREATE TABLE `palavra` (
-  `palavra` varchar(50) NOT NULL,
-  `idpalavra` int(11) NOT NULL,
-  `sinonimo` varchar(200),
-  `significado` varchar(200)
-);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `palavra_aluno`
---
-
-CREATE TABLE `palavra_aluno` (
-  `idpalavra` int(11) NOT NULL,
-  `idusuario` int(6) NOT NULL
-);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `palavra_questao`
---
-
-CREATE TABLE `palavra_questao` (
-  `idpalavra` int(11) NOT NULL,
-  `idquestao` int(11) NOT NULL
-);
+  `nome` varchar(50) COLLATE utf8_bin DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -89,17 +54,14 @@ CREATE TABLE `palavra_questao` (
 
 CREATE TABLE `questao` (
   `idquestao` int(3) NOT NULL,
-  `enunciado` varchar(500) NOT NULL,
-  `opA` varchar(120) NOT NULL,
-  `opB` varchar(120) NOT NULL,
-  `opC` varchar(120) NOT NULL,
-  `opD` varchar(120) NOT NULL,
-  `gabarito` int(11) NOT NULL
-);
-
---
--- Extraindo dados da tabela `questao`
---
+  `enunciado` varchar(500) COLLATE utf8_bin NOT NULL,
+  `opA` varchar(120) COLLATE utf8_bin NOT NULL,
+  `opB` varchar(120) COLLATE utf8_bin NOT NULL,
+  `opC` varchar(120) COLLATE utf8_bin NOT NULL,
+  `opD` varchar(120) COLLATE utf8_bin NOT NULL,
+  `gabarito` int(11) NOT NULL,
+  `TIPO` int(1) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -110,7 +72,7 @@ CREATE TABLE `questao` (
 CREATE TABLE `questao_exercicio` (
   `idexercicio` int(11) NOT NULL,
   `idquestao` int(11) NOT NULL
-);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
 
@@ -119,17 +81,14 @@ CREATE TABLE `questao_exercicio` (
 --
 
 CREATE TABLE `usuario` (
-  `nome` varchar(150) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `senha` varchar(60) NOT NULL,
+  `nome` varchar(150) COLLATE utf8_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8_bin NOT NULL,
+  `senha` varchar(60) COLLATE utf8_bin NOT NULL,
   `datanasc` date NOT NULL,
   `id` int(6) NOT NULL,
   `tipo` int(1) NOT NULL
-);
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Extraindo dados da tabela `usuario`
---
 --
 -- Indexes for dumped tables
 --
@@ -147,26 +106,6 @@ ALTER TABLE `aula`
 --
 ALTER TABLE `exercicio`
   ADD PRIMARY KEY (`idexercicio`);
-
---
--- Indexes for table `palavra`
---
-ALTER TABLE `palavra`
-  ADD PRIMARY KEY (`idpalavra`);
-
---
--- Indexes for table `palavra_aluno`
---
-ALTER TABLE `palavra_aluno`
-  ADD PRIMARY KEY (`idusuario`,`idpalavra`),
-  ADD KEY `idpalavra` (`idpalavra`);
-
---
--- Indexes for table `palavra_questao`
---
-ALTER TABLE `palavra_questao`
-  ADD PRIMARY KEY (`idpalavra`,`idquestao`),
-  ADD KEY `idquestao` (`idquestao`);
 
 --
 -- Indexes for table `questao`
@@ -201,21 +140,6 @@ ALTER TABLE `aula`
 --
 ALTER TABLE `exercicio`
   MODIFY `idexercicio` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `palavra`
---
-ALTER TABLE `palavra`
-  MODIFY `idpalavra` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `palavra_aluno`
---
-ALTER TABLE `palavra_aluno`
-  MODIFY `idpalavra` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `palavra_questao`
---
-ALTER TABLE `palavra_questao`
-  MODIFY `idpalavra` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `questao`
 --
