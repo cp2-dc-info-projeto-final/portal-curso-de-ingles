1f44c6362d68cadad646b2c4ad9412c380b409ed
mileny <telma_evangelista@yahoo.com.br>
2019-11-19 17:48:11 -0300

    Finalizando

--- aluno/uparLevelCtrl.php
+++ aluno/uparLevelCtrl.php
@@ -3,19 +3,19 @@
     require "uparLevel.php";
 
     function upar() {
-        $tipoOld = $_SESSION["tipo"];
-        $email = $_SESSION["email"];
+        $tipoOld=$_SESSION["tipo"];
+        $email=$_SESSION["email"];
         //echo "$tipoOld"; echo "$email";
-        if($tipoOld >3){
+        if($tipoOld>3){
             echo "You are the best ";
 
         }else{
-            if (uparLevel($email) == true){
-                $tipoNovinho = $_SESSION["tipo"];
+            if (uparLevel($email)==true){
+                $tipoNovinho=$_SESSION["tipo"];
                 session_unset();
-                $_SESSION["tipo"]= $tipoNovinho;
-                $_SESSION["email"] = $email;
-                header("Location: alunoView.php");
+                $_SESSION["tipo"]=$tipoNovinho;
+                $_SESSION["email"]=$email;
+                header("Location:alunoView.php");
                 exit();
             }
 
